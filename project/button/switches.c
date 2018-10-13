14465478099cc6916d07d4f076c7533e8b52fb60
Victor Vargas <v.conejo@live.com>
2018-10-13 17:57:35 -0600

    Modified some things to make button from board expansion work with led

--- project/button/switches.c
+++ project/button/switches.c
@@ -7,20 +7,20 @@ char switch_state_down, switch_state_changed; /* effectively boolean */
 static char 
 switch_update_interrupt_sense()
 {
-  char p1val = P1IN;
+  char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
-  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
-  return p1val;
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  return p2val;
 }
 
 void 
 switch_init()			/* setup switch */
 {  
-  P1REN |= SWITCHES;		/* enables resistors for switches */
-  P1IE = SWITCHES;		/* enable interrupts from switches */
-  P1OUT |= SWITCHES;		/* pull-ups for switches */
-  P1DIR &= ~SWITCHES;		/* set switches' bits for input */
+  P2REN |= SWITCHES;		/* enables resistors for switches */
+  P2IE = SWITCHES;		/* enable interrupts from switches */
+  P2OUT |= SWITCHES;		/* pull-ups for switches */
+  P2DIR &= ~SWITCHES;		/* set switches' bits for input */
   switch_update_interrupt_sense();
   led_update();
 }
@@ -28,8 +28,8 @@ switch_init()			/* setup switch */
 void
 switch_interrupt_handler()
 {
-  char p1val = switch_update_interrupt_sense();
-  switch_state_down = (p1val & SW1) ? 0 : 1; /* 0 when SW1 is up */
+  char p2val = switch_update_interrupt_sense();
+  switch_state_down = (p2val & SW1) ? 0 : 1; /* 0 when SW1 is up */
   switch_state_changed = 1;
   led_update();
 }
--- project/button/switches.h
+++ project/button/switches.h
@@ -1,7 +1,7 @@
 #ifndef switches_included
 #define switches_included
 
-#define SW1 BIT3		/* switch1 is p1.3 */
+#define SW1 BIT1		/* switch1 is p2.1 hopefully */
 #define SWITCHES SW1		/* only 1 switch on this board */
 
 void switch_init();
